b20dd4476d26719b721c008da2b51dc61fc0467c
niiyazobug <niiyaz5555@gmail.com>
2022-03-11 19:51:32 +0600

    question section show problem solved

--- js/app.js
+++ js/app.js
@@ -35,16 +35,22 @@ const switchTab = (id) => {
     document.getElementById('posts').style.display = "grid";
     document.getElementById('liked').style.display = "none";
     document.getElementById('reported').style.display = "none";
+    document.getElementById('question').style.display = 'block'
+    document.getElementById('footer').style.display = 'block'
   } else if (id === "liked") {
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
+    document.getElementById('question').style.display = 'none'
+    document.getElementById('footer').style.display = 'none'
 
     displayLikedPosts();
   } else {
     document.getElementById("reported").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("liked").style.display = "none";
+    document.getElementById('question').style.display = 'none'
+    document.getElementById('footer').style.display = 'none'
 
     displayReportedPosts();
   }
